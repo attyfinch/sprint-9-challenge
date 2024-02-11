a7f0164f13b077755d7fafff8c6782699ce44dde
Brad Morris <attyfinch@gmail.com>
2024-02-11 12:36:22 -0800

    acme.py module complete

---
+++ acme.py
@@ -0,0 +1,75 @@
+import random
+
+class Product:
+    def __init__(self, name, price=10, weight=20, flammability=0.5, identifier=random.randint(1000000, 9999999)):
+        self.name = name
+        self.price = price
+        self.weight = weight
+        self.flammability = flammability
+        self.identifier = identifier
+    
+    def stealability(self):
+        ratio = self.price / self.weight
+
+        if ratio < 0.5:
+            return "Not so stealable..."
+        elif ratio < 1.0 and ratio >= 0.5:
+            return "Kinda stealable."
+        else:
+            return "Very stealable!"
+
+    def explode(self):
+        flame_level = self.flammability * self.weight
+        
+        if flame_level < 10:
+            return "...fizzle."
+        elif flame_level >= 10 and flame_level < 50:
+            return "...boom!"
+        else:
+            return "...BABOOM!!"
+
+
+'''
+Make a child class called BoxingGlove that inherits from Product named BoxingGlove and does the following:
+
+Change the default weight to 10 (but leave other defaults unchanged)
+Override the explode method to always return "...it's a glove."
+Add a punch method that returns:
+"That tickles." if the weight is below 5
+"Hey that hurt!" if the weight is greater or equal to 5 but less than 15
+"OUCH!" otherwise
+'''
+
+class BoxingGlove(Product):
+    def __init__(self, name, price=10, weight=10, flammability=0.5, identifier=random.randint(1000000, 9999999)):
+        super().__init__(name, price, weight, flammability, identifier)
+    
+    def explode(self):
+        return "...it's a glove."
+    
+    def punch(self):
+        if self.weight < 5:
+            return "That tickles."
+        elif self.weight >= 5 and self.weight < 15:
+            return "Hey that hurt!"
+        else:
+            return "OUCH!"
+
+
+if __name__ == '__main__':
+    # prod_one = Product('soap')
+    # prod_two = Product('soap', price=20, weight=100)
+    # prod_three = Product('soap', price=20, weight=10)
+
+    soap = Product('soap')
+    print(soap.explode())
+
+    soap.weight = 50
+    print(soap.weight)
+
+    glove = BoxingGlove('test', weight=50)
+    print(glove.explode())
+    print(glove.punch())
+    
+
+
---
+++ acme_report.py
@@ -0,0 +1,21 @@
+import random
+import acme
+
+
+'''
+name should consist of a random adjective from the list: ['Awesome', 'Shiny', 'Impressive', 'Portable', 'Improved'] followed by a space and then a random noun from the list: ['Anvil', 'Catapult' 'Disguise' 'Mousetrap', '???'], e.g. 'Awesome Anvil' and 'Portable Catapult' are both potential names that could be generated
+price and weight should both be random integers from 5 to 100 (inclusive)
+flammability should be a float ranging from 0.0 to 2.5 (inclusive)
+'''
+
+adj = ['Awesome', 'Shiny', 'Impressive', 'Portable', 'Improved']
+noun = ['Anvil', 'Catapult' 'Disguise' 'Mousetrap', '???']
+
+def generate_products(num_products=30):
+    return
+
+
+
+new_product = acme.Product('doll')
+print(new_product)
+
